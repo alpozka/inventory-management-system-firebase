24d6a3cd29cdf8d3bfe61be554b188d5f4989139
alpozka <70730220+alpozka@users.noreply.github.com>
2023-07-07 17:53:37 +0300

    7temmuz v4

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,26 +1,3 @@
-// // import firebase from 'firebase';
-// import { initializeApp } from "firebase/app";
-// // import { getDatabase} from "firebase/database";
-// import { getFirestore } from "firebase/firestore";
-// import { getAuth } from "@firebase/auth";
-// import { getStorage } from "firebase/storage"
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDDsyqF-TQXhMNPfhLRsETsssWm5iM6uFU",
-//     authDomain: "envanter-demo.firebaseapp.com",
-//     projectId: "envanter-demo",
-//     storageBucket: "envanter-demo.appspot.com",
-//     messagingSenderId: "178968411696",
-//     appId: "1:178968411696:web:bae11be4e61b657bde37f5",
-//     measurementId: "G-LXHLWYVDNX"
-// };
-// const app = initializeApp(firebaseConfig);
-
-
-// export const auth = getAuth(); 
-// export const storage = getStorage(app);
-// export const db = getFirestore(app);
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "@firebase/auth";
