ac489888757ce9d155e72949883009ace0e0dc70
alpozka <70730220+alpozka@users.noreply.github.com>
2023-07-10 11:53:42 +0300

    10temmuz_calısan_sorgu

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -8,236 +8,256 @@ import { Link } from 'react-router-dom';
 import { collection, query, orderBy, startAt, endAt, getDocs, getDoc, doc } from 'firebase/firestore';
 import { db } from './firebase';
 import '../styles/HomePage.css';
-import PersonForm from './PersonForm'; 
+import PersonForm from './PersonForm';
 import ProductForm from './ProductForm';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-
 const HomePage = () => {
-    const [openPersonForm, setOpenPersonForm] = useState(false); 
-    const [openProductForm, setOpenProductForm] = useState(false);
-    const [searchId, setSearchId] = useState('');
-    const [searchResult, setSearchResult] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [assignedPeople, setAssignedPeople] = useState({});
+  const [openPersonForm, setOpenPersonForm] = useState(false);
+  const [openProductForm, setOpenProductForm] = useState(false);
+  const [searchId, setSearchId] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [persons, setPersons] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    const handleOpenPersonForm = () => {
-        setOpenPersonForm(true);
-    };
+  const fetchPersons = async () => {
+    const personsRef = collection(db, 'people');
+    const personsSnapshot = await getDocs(personsRef);
+    const personsList = personsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    setPersons(personsList);
+  };
 
-    const handleClosePersonForm = () => {
-        setOpenPersonForm(false);
-    };
+  const fetchProducts = async () => {
+    const productsRef = collection(db, 'products');
+    const productsSnapshot = await getDocs(productsRef);
+    const productsList = productsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    setProducts(productsList);
+  };
 
-    const handleOpenProductForm = () => {
-        setOpenProductForm(true);
-    };
+  useEffect(() => {
+    fetchPersons();
+    fetchProducts();
+  }, []);
 
-    const handleCloseProductForm = () => {
-        setOpenProductForm(false);
-    };
+  const handleOpenPersonForm = () => {
+    setOpenPersonForm(true);
+  };
 
-    const handleSearch = async () => {
-        if (searchId.length < 2) {
-          setError('En az 2 karakter girin');
-          return;
-        }
-      
-        setError('');
-        setIsLoading(true);
-      
-        // Create empty array to store all results
-        const results = [];
-      
-        // Define fields to search in 'people' collection
-        const peopleFieldsToSearch = ['idLowerCase', 'nameLowerCase', 'titleLowerCase', 'surnameLowerCase'];
-      
-        // Search in 'people' collection
-        for (let field of peopleFieldsToSearch) {
-          const peopleRef = collection(db, 'people');
-          const peopleQuery = query(
-            peopleRef,
-            orderBy(field),
-            startAt(searchId.toLowerCase()),
-            endAt(searchId.toLowerCase() + '\uf8ff')
-          );
-          const peopleQuerySnapshot = await getDocs(peopleQuery);
-      
-          peopleQuerySnapshot.forEach((doc) => {
-            const data = doc.data();
-            results.push({
-              ...data,
-              type: 'person',
-              assignedPersonName: data.assignedPersonId ? assignedPeople[data.assignedPersonId]?.name : null,
-              assignedPersonSurname: data.assignedPersonId ? assignedPeople[data.assignedPersonId]?.surname : null,
-            });
-          });
-        }
-      
-        // Define fields to search in 'products' collection
-        const productFieldsToSearch = ['idLowerCase', 'brandLowerCase', 'descriptionLowerCase', 'modelLowerCase'];
-      
-        // Search in 'products' collection
-        for (let field of productFieldsToSearch) {
-          const productRef = collection(db, 'products');
-          const productQuery = query(
-            productRef,
-            orderBy(field),
-            startAt(searchId.toLowerCase()),
-            endAt(searchId.toLowerCase() + '\uf8ff')
-          );
-          const productQuerySnapshot = await getDocs(productQuery);
-      
-          productQuerySnapshot.forEach((doc) => {
-            results.push({ ...doc.data(), type: 'product' });
-          });
-        }
-      
-        // Get assigned person names
-        const assignedPersonIds = [];
-        const assignedPeopleData = {};
-        for (let result of results) {
-          if (!result.assignedPersonId || typeof result.assignedPersonId !== 'string') {
-            continue;
-          }
-          if (!assignedPersonIds.includes(result.assignedPersonId)) {
-            assignedPersonIds.push(result.assignedPersonId);
-            const personRef = doc(db, 'people', result.assignedPersonId);
-            const personDoc = await getDoc(personRef);
-            if (personDoc.exists()) {
-              const assignedPersonName = personDoc.data().name;
-              const assignedPersonSurname = personDoc.data().surname;
-              assignedPeopleData[result.assignedPersonId] = {
-                name: assignedPersonName,
-                surname: assignedPersonSurname,
-              };
+  const handleClosePersonForm = () => {
+    setOpenPersonForm(false);
+  };
+
+  const handleOpenProductForm = () => {
+    setOpenProductForm(true);
+  };
+
+  const handleCloseProductForm = () => {
+    setOpenProductForm(false);
+  };
+
+  const handleSearch = async () => {
+    if (searchId.length < 2) {
+      setError('En az 2 karakter girin');
+      return;
+    }
+
+    setError('');
+    setIsLoading(true);
+
+    // Create empty array to store all results
+    const results = [];
+
+    // Define fields to search in 'people' collection
+    const peopleFieldsToSearch = ['idLowerCase', 'nameLowerCase', 'titleLowerCase', 'surnameLowerCase'];
+
+    // Search in 'people' collection
+    for (let field of peopleFieldsToSearch) {
+      const peopleRef = collection(db, 'people');
+      const peopleQuery = query(
+        peopleRef,
+        orderBy(field),
+        startAt(searchId.toLowerCase()),
+        endAt(searchId.toLowerCase() + '\uf8ff')
+      );
+      const peopleQuerySnapshot = await getDocs(peopleQuery);
+
+      peopleQuerySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const assignedProducts = [];
+        if (Array.isArray(data.assignedDeviceOrSoftwareId)) {
+          data.assignedDeviceOrSoftwareId.forEach((productId) => {
+            const assignedProduct = products.find((product) => product.id === productId);
+            if (assignedProduct) {
+              assignedProducts.push(`${assignedProduct.brand} ${assignedProduct.model} (ID: ${productId})`);
             }
+          });
+        } else {
+          const assignedProduct = products.find((product) => product.id === data.assignedDeviceOrSoftwareId);
+          if (assignedProduct) {
+            assignedProducts.push(`${assignedProduct.brand} ${assignedProduct.model} (ID: ${data.assignedDeviceOrSoftwareId})`);
           }
         }
-        setAssignedPeople(assignedPeopleData);
-      
-        if (results.length > 0) {
-          setSearchResult(results);
+
+        results.push({
+          ...data,
+          type: 'person',
+          assignedProducts: assignedProducts.join(', ')
+        });
+      });
+    }
+
+    // Define fields to search in 'products' collection
+    const productFieldsToSearch = ['idLowerCase', 'brandLowerCase', 'descriptionLowerCase', 'modelLowerCase'];
+
+    // Search in 'products' collection
+    for (let field of productFieldsToSearch) {
+      const productRef = collection(db, 'products');
+      const productQuery = query(
+        productRef,
+        orderBy(field),
+        startAt(searchId.toLowerCase()),
+        endAt(searchId.toLowerCase() + '\uf8ff')
+      );
+      const productQuerySnapshot = await getDocs(productQuery);
+
+      productQuerySnapshot.forEach((doc) => {
+        const productData = doc.data();
+        const productAssignedPersons = [];
+        if (Array.isArray(productData.assignedPersonId)) {
+          productData.assignedPersonId.forEach((personId) => {
+            const assignedPerson = persons.find((person) => person.id === personId);
+            if (assignedPerson) {
+              productAssignedPersons.push(`${assignedPerson.name} ${assignedPerson.surname} (ID: ${personId})`);
+            }
+          });
         } else {
-          console.log('No such document!');
-          setSearchResult([{ type: 'error', message: 'No such document!' }]);
+          // If it's not an array, it's probably a single id, handle this case
+          const assignedPerson = persons.find((person) => person.id === productData.assignedPersonId);
+          if (assignedPerson) {
+            productAssignedPersons.push(`${assignedPerson.name} ${assignedPerson.surname} (ID: ${productData.assignedPersonId})`);
+          }
         }
-        setIsLoading(false);
-      };
-      
 
-    return (
-        <Container>
-            <div className="head">
-                <h1>Hoşgeldiniz!</h1>
-                <p>Bu, Envanter Takip Sistemi'nin ana sayfasıdır.</p>
-            </div>
-            <Box display="flex" justifyContent="center">
-                <TextField 
-                    label="Sorgulama" 
-                    variant="outlined" 
-                    style={{ width: '70%' }} 
-                    value={searchId} 
-                    onChange={e => setSearchId(e.target.value)} 
-                    error={!!error}
-                    helperText={error}
-                    onKeyPress={(event) => {
-                        if (event.key === 'Enter') {
-                            event.preventDefault();
-                            handleSearch();
-                        }
-                    }}
-                />    
-            </Box>
-            <div className="button-sorgu">
-                <Box m={2}>
-                    <Button variant="contained" color="primary" fullWidth className="my-button" onClick={handleSearch}>Sorgula</Button>
-                </Box>
-            </div>
+        results.push({
+          ...productData,
+          type: 'product',
+          assignedPersonName: productAssignedPersons.join(', ') // Here the persons are joined by comma
+        });
+      });
+    }
 
-            {isLoading ? (
-  <Box display="flex" justifyContent="center" m={3}>
-    <CircularProgress />
-  </Box>
-) : searchResult && searchResult.length > 0 ? (
-  <div className="results-grid">
-    {searchResult.map((result, index) => (
-      <Card className="result-card" key={index}>
-        <CardContent>
-          {result.type === 'person' ? (
-            <div>
-              <Typography variant="h5" component="div">Kişi</Typography>
-              <Typography>Ad: {result.name}</Typography>
-              <Typography>Soyad: {result.surname}</Typography>
-              <Typography>Ünvan: {result.title}</Typography>
-              <Typography>İşe giriş tarihi: {result.joiningDate}</Typography>
-              <Typography>Sisteme Kayıt Tarihi: {result.registrationDate}</Typography>
-              <Typography>Atanan Ürün Id: {result.assignedDeviceOrSoftwareId}</Typography>
-              <Typography>Açıklama: {result.description}</Typography>
-              <Typography>ID: {result.id}</Typography>
-            </div>
-          ) : result.type === 'product' ? (
-            <div>
-              <Typography variant="h5" component="div">Ürün</Typography>
-              <Typography>Marka: {result.brand}</Typography>
-              <Typography>Model: {result.model}</Typography>
-              <Typography>Açıklama: {result.description}</Typography>
-              <Typography>Satın Alınma Tarihi: {result.purchaseDate}</Typography>
-              <Typography>Sisteme Kayıt Tarihi: {result.registerDate}</Typography>
-              <Typography>Fiyat: {result.price}</Typography>
-              <Typography>
-  Atanan Kişi Id:{' '}
-  {Array.isArray(result.assignedPersonId)
-    ? result.assignedPersonId
-        .map((personId) => `${personId} (${assignedPeople[personId]?.name} ${assignedPeople[personId]?.surname})`)
-        .join(', ')
-    : result.assignedPersonId
-    ? `${result.assignedPersonId} (${assignedPeople[result.assignedPersonId]?.name} ${assignedPeople[result.assignedPersonId]?.surname})`
-    : ''}
-</Typography>
-              <Typography>ID: {result.id}</Typography>
-            </div>
-          ) : (
-            <div>
-              <Typography>{result.message}</Typography>
-            </div>
-          )}
-        </CardContent>
-      </Card>
-    ))}
-  </div>
-) : null}
+    if (results.length === 0) {
+      setError('Sonuç bulunamadı');
+    } else {
+      setSearchResult(results);
+    }
 
+    setIsLoading(false);
+  };
 
-            <div className="button-group">
-                <Box m={2}>
-                    <Button variant="contained" color="secondary" fullWidth className="my-button" onClick={handleOpenPersonForm}>Kişi Ekle</Button>
-                </Box>
+  return (
+    <Container>
+      <div className="head">
+        <h1>Hoşgeldiniz!</h1>
+        <p>Bu, Envanter Takip Sistemi'nin ana sayfasıdır.</p>
+      </div>
+      <Box display="flex" justifyContent="center">
+        <TextField
+          label="Sorgulama"
+          variant="outlined"
+          style={{ width: '70%' }}
+          value={searchId}
+          onChange={e => setSearchId(e.target.value)}
+          error={!!error}
+          helperText={error}
+          onKeyPress={(event) => {
+            if (event.key === 'Enter') {
+              event.preventDefault();
+              handleSearch();
+            }
+          }}
+        />
+      </Box>
+      <div className="button-sorgu">
+        <Box m={2}>
+          <Button variant="contained" color="primary" fullWidth className="my-button" onClick={handleSearch}>Sorgula</Button>
+        </Box>
+      </div>
 
-                <Box m={2}>
-                    <Button variant="contained" color="secondary" fullWidth className="my-button" onClick={handleOpenProductForm}>Ürün Ekle</Button>
-                </Box>
+      {isLoading ? (
+        <Box display="flex" justifyContent="center" m={3}>
+          <CircularProgress />
+        </Box>
+      ) : searchResult && searchResult.length > 0 ? (
+        <div className="results-grid">
+          {searchResult.map((result, index) => (
+            <Card className="result-card" key={index}>
+              <CardContent>
+                {result.type === 'person' ? (
+                  <div>
+                    <Typography variant="h5" component="div">Kişi</Typography>
+                    <Typography>Ad: {result.name}</Typography>
+                    <Typography>Soyad: {result.surname}</Typography>
+                    <Typography>Ünvan: {result.title}</Typography>
+                    <Typography>İşe giriş tarihi: {result.joiningDate}</Typography>
+                    <Typography>Sisteme Kayıt Tarihi: {result.registrationDate}</Typography>
+                    <Typography>Atanan Ürünler: {result.assignedProducts}</Typography>
+                    <Typography>ID: {result.id}</Typography>
+                  </div>
+                ) : result.type === 'product' ? (
+                  <div>
+                    <Typography variant="h5" component="div">Ürün</Typography>
+                    <Typography>Marka: {result.brand}</Typography>
+                    <Typography>Model: {result.model}</Typography>
+                    <Typography>Açıklama: {result.description}</Typography>
+                    <Typography>Satın Alınma Tarihi: {result.purchaseDate}</Typography>
+                    <Typography>Sisteme Kayıt Tarihi: {result.registerDate}</Typography>
+                    <Typography>Fiyat: {result.price}</Typography>
+                    <Typography>Atanan Kişi: {result.assignedPersonName}</Typography>
+                    <Typography>ID: {result.id}</Typography>
+                  </div>
+                ) : (
+                  <div>
+                    <Typography variant="h5" component="div">Bilinmeyen sonuç tipi: {result.type}</Typography>
+                    <Typography>Data: {JSON.stringify(result, null, 2)}</Typography>
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      ) : null}
 
-                <Box m={2}>
-                    <Link to="/people">
-                        <Button variant="contained" color="secondary" fullWidth className="my-button">Kişiler</Button>
-                    </Link>  
-                </Box>
+      <div className="button-group">
+        <Box m={2}>
+          <Button variant="contained" color="secondary" fullWidth className="my-button" onClick={handleOpenPersonForm}>Kişi Ekle</Button>
+        </Box>
 
-                <Box m={2}>
-                    <Link to="/products"> 
-                        <Button variant="contained" color="secondary" fullWidth className="my-button">Ürünler</Button>
-                    </Link> 
-                </Box>
-            </div>
+        <Box m={2}>
+          <Button variant="contained" color="secondary" fullWidth className="my-button" onClick={handleOpenProductForm}>Ürün Ekle</Button>
+        </Box>
 
-            <PersonForm open={openPersonForm} handleClose={handleClosePersonForm} />  
-            <ProductForm open={openProductForm} handleClose={handleCloseProductForm} />
-        </Container>
-    );
-}
+        <Box m={2}>
+          <Link to="/people">
+            <Button variant="contained" color="secondary" fullWidth className="my-button">Kişiler</Button>
+          </Link>
+        </Box>
+
+        <Box m={2}>
+          <Link to="/products">
+            <Button variant="contained" color="secondary" fullWidth className="my-button">Ürünler</Button>
+          </Link>
+        </Box>
+      </div>
+
+      <PersonForm open={openPersonForm} handleClose={handleClosePersonForm} />
+      <ProductForm open={openProductForm} handleClose={handleCloseProductForm} />
+    </Container>
+  );
+};
 
 export default HomePage;
